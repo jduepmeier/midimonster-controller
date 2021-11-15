135bd03ce30ec68dbdd2841ebfaec7c3041309a1
Jan Düpmeier <j.duepmeier@googlemail.com>
2021-11-15 15:48:29 +0100

    make loglevel configurable from command line

--- cmd/midimonster-controller/main.go
+++ cmd/midimonster-controller/main.go
@@ -10,24 +10,36 @@ import (
 
 type opts struct {
 	ConfigPath string `short:"c" long:"config" description:"path to config path"`
+	LogLevel   string `short:"l" long:"loglevel" description:"loglevel"`
 }
 
 func main() {
-	opts := opts{}
+	opts := opts{
+		LogLevel: "warn",
+	}
 	logger := zerolog.New(os.Stderr)
 	_, err := flags.Parse(&opts)
 	if err != nil {
 		return
 	}
 
+	level, err := zerolog.ParseLevel(opts.LogLevel)
+	if err != nil {
+		logger.Err(err).Msg("cannot parse loglevel")
+		return
+	}
+	zerolog.SetGlobalLevel(level)
+
 	config, err := midimonster.ReadConfig(opts.ConfigPath)
 	if err != nil {
 		logger.Err(err).Msg("cannot read config")
+		return
 	}
 
 	controller, err := midimonster.NewController(config, logger)
 	if err != nil {
 		logger.Err(err).Msg("cannot create controller")
+		return
 	}
 	err = controller.Serve()
 	if err != nil {
